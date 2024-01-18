2323a4ee56a4bfe580689ee170d839e4c2ca9ce9
PRAVEEN KUMAR <115071711+Praveenkumar07007@users.noreply.github.com>
2024-01-18 12:20:40 +0000

    added some first files

---
+++ day_1/first.js
@@ -0,0 +1,3 @@
+//Hello World: Print “Hello, World!” to the console.
+
+console.log("Hello world")
---
+++ day_1/sumofnumber.js
@@ -0,0 +1,5 @@
+//Sum of Two Numbers: Add two numbers and print the result.
+
+let x=2;
+let y=3;
+console.log(x+y);
